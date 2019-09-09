610dedaca5154d56903f1b801cbd45cab98e57b8
suaas21 <sagor@appscode.com>
2019-09-09 09:45:13 +0600

    fix restore PVC

--- pkg/controller/inputs.go
+++ pkg/controller/inputs.go
@@ -128,6 +128,9 @@ func (c *StashController) inputsForRestoreTarget(target *api.RestoreTarget) map[
 		} else {
 			inputs[apis.TargetMountPath] = apis.StashDefaultMountPath
 		}
+		if target.VolumeClaimTemplates[0].Name != "" && target.Ref.Name == "" {
+			inputs[apis.TargetName] = target.VolumeClaimTemplates[0].ObjectMeta.Name
+		}
 	}
 	return inputs
 }
